fcb4383a224806bd1ee3f26a69f98fd104236e38
Naveena <naveena.n76@gmail.com>
2019-01-04 10:33:31 +0000

    techtest_Exercise4

--- src/main/java/com/karhoo/qa/util/TestUtil.java
+++ src/main/java/com/karhoo/qa/util/TestUtil.java
@@ -2,6 +2,6 @@ package com.karhoo.qa.util;
 
 public class TestUtil {
 	public static long PAGE_LOAD_TIMEOUT = 20;
-	public static long IMPLICIT_WAIT = 10;
+	public static long IMPLICIT_WAIT = 20;
 
 }
